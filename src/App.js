7be5d09f5134bf24f5bd966c43ee672df3d5cf0a
清川　裕太郎 <yuu7353@gmail.com>
2020-06-16 23:23:26 +0900

    feat: TodoListComponentの作成

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import TodoList from './TodoList';
 
 class App extends React.Component {
   render() {
+    const list = ['プログラミング', 'お買い物'];
+
     return (
+      <div>
       <h1>TODOアプリ</h1>
+      <TodoList list={list} />
+      </div>
     );
   }
 }
